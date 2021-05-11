da8f0a916f35451507202e379bc941ad4a24ea07
E42-d <marc.desideri@hotmail.fr>
2021-05-11 14:20:53 +0200

    added Card component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Background from './main/Background';
+import GameGrid from './main/Game/GameGrid';
 
 function App() {
   return (
     <div>
- 
+ <Background/>
+ <GameGrid/>
     </div>
   );
 }
---
+++ src/main/Background.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import background from "./background.mp4"
+function Background(props) {
+    return (
+        <div>
+                        <video
+                autoPlay
+                loop 
+                muted
+                style={{
+                    position:"absolute",
+                    width: "100%",
+                    left: "50%",
+                    top: "50%",
+                    height: "100%",
+                    objectFit:"cover",
+                    transform:"translate(-50%, -50%)",
+                    zIndex:"-1"
+
+                }}>
+<source src={background} type="video/mp4"/>
+            </video>
+        </div>
+    );
+}
+
+export default Background;
