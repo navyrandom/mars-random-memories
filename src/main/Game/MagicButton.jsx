ed5cbda24e5901a65032627d74259dc6c81b0192
antoinedupuy50 <antoinedupuy50@gmail.com>
2021-05-12 13:40:45 +0200

    magic button

---
+++ src/main/Game/MagicButton.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+import Game from '../Morpion/Game';
+
+const useStyles = makeStyles(() => ({
+  button: {
+    color: 'white',
+    // display: 'none',
+    fontSize: '60px',
+    position: 'absolute',
+    bottom: '0px',
+    // visibility: 'hidden'
+    // opacity: '0'
+  },
+}));
+
+function ButtonMorpion() {
+  const classes = useStyles();
+
+  const [showCongrats, setShowCongrats] = useState(false);
+
+  const handleClick = () => {
+    window.alert('YOU ARE FUCKING AWESOME LIKE THIS WEBSITE');
+    // setShowCongrats(!showCongrats);
+  };
+
+  return (
+    <button onClick={handleClick} className={classes.button}>
+      MAGIC BUTTON
+    </button>
+  );
+}
+
+export default ButtonMorpion;
