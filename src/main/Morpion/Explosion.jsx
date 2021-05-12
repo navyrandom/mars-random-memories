929238a0109e832c6008394d19b3895826cae781
Nicolas <nicolasgeneste@gmail.com>
2021-05-12 14:32:35 +0200

    thelast

---
+++ src/main/Morpion/Explosion.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Explosion(props) {
+  return (
+    <div>
+      <h1>big bang</h1>
+      <img src="/apocalypse.jpg" alt="fin du monde" />
+    </div>
+  );
+}
+
+export default Explosion;
